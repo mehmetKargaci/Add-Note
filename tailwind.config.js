64420010f5145dff45a4e4697003a7d06d0bd028
mehmetKargaci <mehmetkargaci34@gmail.com>
2024-07-17 05:46:48 +0200

    tailwindcss added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "lato":["lato","sans-serif"],
+        "roboto":["roboto", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
